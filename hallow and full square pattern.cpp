c7fecc883c1627384927eba4a04389f87bb7b47a
bhavana192111387 <112713393+bhavana192111387@users.noreply.github.com>
2022-09-10 10:12:23 +0530

    Add files via upload

---
+++ hallow and full square pattern.cpp
@@ -0,0 +1,33 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i, j, side;  
+    cout << "Enter Side of a Hollow Square = ";
+    cin >> side;
+    cout << "Hollow Square Star Pattern\n";
+
+    for(i = 0; i < side; i++) 
+    {
+    	for(j = 0; j < side; j++)
+        {
+            if (i == 0 || i == side - 1 || j == 0 || j == side - 1) 
+            {
+                cout << "*" << " ";
+            }
+            else 
+            {
+                cout << "  ";
+            } 
+        }
+        cout << "\n";
+    }
+  int size = 5;
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {cout << "*";
+    }
+    cout << "\n";
+  }		
+ 	return 0;
+}
