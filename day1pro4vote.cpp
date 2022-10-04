573ceb7df5cb318b42b13e2c4d3d3b95898288a1
bhavana192111387 <112713393+bhavana192111387@users.noreply.github.com>
2022-10-04 10:11:56 +0530

    Add files via upload

---
+++ day1pro4vote.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"Enter your age: "<<endl;
+	cin>>age;
+	if(age>=7)
+	{
+		cout<<"you are allowed to  vote"<<endl;
+	}
+	else
+	{
+		cout<<"invalid && you are not allowed to vote"<<endl;
+}
+}
+
